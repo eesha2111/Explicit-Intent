fbb4e7ad0ea9d15a21135e1af411edb806662793
Eesha Ahmed <eeshaahmed2111@gmail.com>
2024-04-15 05:04:52 +0500

    Initial commit

---
+++ app/src/main/java/com/example/explicitintent/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.explicitintent;
+
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.SecondActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnIntent=findViewById(R.id.button);
+
+        btnIntent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
